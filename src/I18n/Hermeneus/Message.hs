cf2e7661565280aa0dac1bb378cc08a957745510
SAKAMOTO Noriaki <mrty.ityt.pt@gmail.com>
2018-04-30 12:39:21 +0900

    Support escaped curly braces in TranslationTemplate

--- src/I18n/Hermeneus/Message.hs
+++ src/I18n/Hermeneus/Message.hs
@@ -2,22 +2,27 @@
 
 module I18n.Hermeneus.Message where
 
+import Control.Applicative ((<$>))
+import Data.Functor
 import Data.Maybe
+import Data.Semigroup (Semigroup, (<>))
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.String
 
 import Text.Parsec
 
 import I18n.Hermeneus.Prim
 
+-- ToDo: Provide escape sequence in word reference
 
 parseTranslationTemplate :: Stream s m Char => ParsecT s u m TranslationTemplate
 parseTranslationTemplate = TranslationTemplate <$> many parseTranslationHank
 
 parseTranslationHank :: Stream s m Char => ParsecT s u m TranslationHank
-parseTranslationHank = parsePlaceholder <|> parseTranslatedString
+parseTranslationHank = parseTranslatedString <|> parsePlaceholder
 
 parsePlaceholder :: Stream s m Char => ParsecT s u m TranslationHank
 parsePlaceholder = do
@@ -28,7 +33,13 @@ parsePlaceholder = do
   return $ Placeholder (wRef, exprs)
 
 parseTranslatedString :: Stream s m Char => ParsecT s u m TranslationHank
-parseTranslatedString = fmap TranslatedString $ many1 $ noneOf "{}"
+parseTranslatedString = TranslatedString <$> many1 parseTranslatedStringChar
+
+parseTranslatedStringChar :: Stream s m Char => ParsecT s u m Char
+parseTranslatedStringChar = try escapedBrace <|> noneOf "{}"
+  where
+    escapedBrace :: Stream s m Char => ParsecT s u m Char
+    escapedBrace = (string "{{" $> '{') <|> (string "}}" $> '}')
 
 parseWordReference :: Stream s m Char => ParsecT s u m WordReference
 parseWordReference = parseWord <|> parseNumber
--- test/I18n/Hermeneus/MessageSpec.hs
+++ test/I18n/Hermeneus/MessageSpec.hs
@@ -11,6 +11,11 @@ import I18n.Hermeneus.Message
 
 unit_parseTranslationHank = do
   parse parseTranslationHank "" "abc" @?= (Right $ TranslatedString "abc")
+  parse parseTranslationHank "" "a{{b" @?= (Right $ TranslatedString "a{b")
+  parse parseTranslationHank "" "{{" @?= (Right $ TranslatedString "{")
+  parse parseTranslationHank "" "{{1" @?= (Right $ TranslatedString "{1")
+  parse parseTranslationHank "" "{{1}}" @?= (Right $ TranslatedString "{1}")
+  parse parseTranslationHank "" "1}}" @?= (Right $ TranslatedString "1}")
   parse parseTranslationHank "" "{1}" @?= (Right $ Placeholder (PlaceholderNumber 1, []))
   parse parseTranslationHank "" "{0:number#2}" @?= (Right $ Placeholder (PlaceholderNumber 0, [FeatureConstraintExpr "number" $ ConcordWord $ PlaceholderNumber 2]))
   parse parseTranslationHank "" "{*to:number#2}" @?= (Right $ Placeholder (WordKey "to", [FeatureConstraintExpr "number" $ ConcordWord $ PlaceholderNumber 2]))
