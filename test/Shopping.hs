cafa1d698828c7d36f4211bc513aa17900982d57
SAKAMOTO Noriaki <mrty.ityt.pt@gmail.com>
2018-05-30 19:33:35 +0900

    Clean up

--- test/Shopping.hs
+++ test/Shopping.hs
@@ -4,7 +4,7 @@ import Control.Exception
 import Control.Monad
 import qualified Data.Map as M
 import qualified Data.Yaml as Y
-import I18n.Hermeneus.Aeson
+import I18n.Hermeneus.Aeson ()
 import I18n.Hermeneus.Engine (translateMessage)
 import I18n.Hermeneus.Prim
 import System.FilePath
@@ -24,19 +24,21 @@ translationResource = do
     return (locale, s)
   return $ M.fromList translationSets
 
+unit_translationResource :: IO ()
 unit_translationResource = do
   db <- translationResource
   M.null db @?= False
 
+unit_translations :: IO ()
 unit_translations = do
   db <- translationResource
   -- translateMessage :: Database -> Locale -> MessageKey -> [MessageArg] -> Either String String
   let context = "com.example.shopping.buy"
-  let msgBought l n w p = translateMessage db l (MessageKey "Bought {0} {1:number#0}." context) [ArgNumber n, ArgWord w p context]
-  msgBought "en" 1 "car" "cars" @?= Right "Bought 1 car."
-  msgBought "en" 2 "car" "cars" @?= Right "Bought 2 cars." -- ToDo: Fix this
-  msgBought "en" 2 "book" "books" @?= Right "Bought 2 books."
-  -- ToDo: Fix those cases
-  msgBought "ja" 1 "car" "cars" @?= Right "1台の車を買った。"
-  msgBought "ja" 1 "car" "cars" @?= Right "1台の車を買った。"
-  msgBought "ja" 1 "book" "books" @?= Right "1冊の本を買った。"
+  let msgThing w p = ArgWord w p context
+  let msgBought l n w = translateMessage db l (MessageKey "Bought {0} {1:number#0}." context) [ArgNumber n, w]
+  msgBought "en" 1 (msgThing "car"  "cars" ) @?= Right "Bought 1 car."
+  msgBought "en" 2 (msgThing "car"  "cars" ) @?= Right "Bought 2 cars." -- ToDo: Fix this
+  msgBought "en" 2 (msgThing "book" "books") @?= Right "Bought 2 books."
+  msgBought "ja" 1 (msgThing "car"  "cars" ) @?= Right "1台の車を買った。"
+  msgBought "ja" 1 (msgThing "car"  "cars" ) @?= Right "1台の車を買った。"
+  msgBought "ja" 1 (msgThing "book" "books") @?= Right "1冊の本を買った。"
