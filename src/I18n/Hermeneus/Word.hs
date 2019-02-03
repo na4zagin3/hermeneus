0d1eb83c12c64aec1aff0f690842e08711fa4174
SAKAMOTO Noriaki <mrty.ityt.pt@gmail.com>
2019-02-03 21:22:31 +0900

    Add string representation of Word

---
+++ src/I18n/Hermeneus/Word.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE FlexibleContexts, OverloadedStrings, PartialTypeSignatures #-}
+module I18n.Hermeneus.Word where
+
+import Control.Applicative ((<$>))
+import Data.Functor
+import Data.List (intersperse)
+import Data.Maybe
+import Data.Semigroup (Semigroup, (<>))
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.String
+
+import Text.Parsec
+
+import I18n.Hermeneus.Message( parseFeatureId, parseFeatureValue )
+import I18n.Hermeneus.Prim
+
+normalize :: LocalizedWord -> LocalizedWord
+normalize (LocalizedWord e ts) = LocalizedWord (normalizeFeatureEnv e) (map normalizeTranslation ts)
+
+normalizeFeatureEnv :: FeatureEnv -> FeatureEnv
+normalizeFeatureEnv (FeatureEnv fe) = FeatureEnv $ M.filterWithKey f fe
+  where
+    f "" _ = False
+    f _ "" = False
+    f _ _ = True
+
+normalizeTranslation :: (FeatureCondition, String) -> (FeatureCondition, String)
+normalizeTranslation (FeatureCondition cs, w) = (FeatureCondition cs', w)
+  where
+    cs' = M.filterWithKey f cs
+    f "" _ = False
+    f _ "" = False
+    f _ _ = True
+
+parseLocalizedWord :: Stream s m Char => ParsecT s u m LocalizedWord
+parseLocalizedWord = do
+  featureEnv <- parseFeatureEnv
+  string "~"
+  features <- parseFeature`sepBy` (string ";")
+  return $ LocalizedWord featureEnv features
+
+printLocalizedWord :: (IsString s, Semigroup s, Monoid s) => LocalizedWord -> s
+printLocalizedWord (LocalizedWord e fs) = printFeatureEnv e <> "~" <> mconcat (intersperse (fromString ";") ts)
+  where
+    ts = map printFeature fs
+
+parseFeatureEnv :: Stream s m Char => ParsecT s u m FeatureEnv
+parseFeatureEnv = do
+  entries <- parseFeatureEnvEntry `sepBy` (string ",")
+  return . FeatureEnv $ M.fromList entries
+
+parseFeatureEnvEntry :: Stream s m Char => ParsecT s u m (FeatureId, FeatureValue)
+parseFeatureEnvEntry = do
+  i <- parseFeatureId
+  string "="
+  v <- parseFeatureValue
+  return $ (i, v)
+
+printFeatureEnv :: (IsString s, Semigroup s, Monoid s) => FeatureEnv -> s
+printFeatureEnv (FeatureEnv fe ) = mconcat . intersperse (fromString ",") . map printEntry . M.toList $ fe
+  where
+    printEntry (i, v) = mconcat [ fromString i, fromString "=", fromString v]
+
+parseFeature :: Stream s m Char => ParsecT s u m (FeatureCondition, String)
+parseFeature = do
+  cond <- parseFeatureCondition
+  string ":"
+  string "\""
+  w <- (many $ (try parseEscaped <|> noneOf "\""))
+  string "\""
+  return (cond, w)
+  where
+    parseEscaped = do
+      string "\\"
+      (string "\\" $> '\\') <|> (string "\"" $> '"')
+
+printFeature :: (IsString s, Semigroup s, Monoid s) => (FeatureCondition, String) -> s
+printFeature (cond, w) = mconcat
+    [ printFeatureCondition cond
+    , fromString ":\""
+    , fromString (concatMap escape w)
+    , fromString "\""
+    ]
+  where
+    escape '\\' = "\\\\"
+    escape '"' = "\\\""
+    escape x = [x]
+
+parseFeatureCondition :: Stream s m Char => ParsecT s u m FeatureCondition
+parseFeatureCondition = do
+  entries <- parseFeatureEnvEntry `sepBy` (string ",")
+  return . FeatureCondition $ M.fromList entries
+
+printFeatureCondition :: (IsString s, Semigroup s, Monoid s) => FeatureCondition -> s
+printFeatureCondition (FeatureCondition m) = mconcat . intersperse (fromString ",") . map printEntry $ M.toList m
+  where
+    printEntry (i, v) = mconcat [ fromString i, fromString "=", fromString v]
---
+++ test/I18n/Hermeneus/WordSpec.hs
@@ -0,0 +1,19 @@
+module I18n.Hermeneus.WordSpec where
+
+import qualified Data.Map as M
+import Data.Text (Text)
+import Text.Parsec
+
+import I18n.Hermeneus.Prim
+import I18n.Hermeneus.Word
+
+import Generic.Random
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import I18n.Hermeneus.ArbitraryInstances
+
+
+prop_word_parser_section :: LocalizedWord -> Bool
+prop_word_parser_section e = (parse parseLocalizedWord "parse expression" (printLocalizedWord e' :: Text)) == Right e'
+  where
+    e' = normalize e
